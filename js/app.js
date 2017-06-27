e11ecbaa2d3ab36dbc54c081bb44a4c8a10193da
Tsigereda <tsigeredasfw@gmail.com>
2017-06-27 16:00:29 +0200

    submitted version

--- js/app.js
+++ js/app.js
@@ -14,9 +14,8 @@ var Enemy = function(x,y,z) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+   
+    if(this.x == player.x && this.y==player.y) console.log("overlapp");
     if(this.x < 505){
 
     this.x +=(this.speed*dt);
@@ -26,19 +25,14 @@ Enemy.prototype.update = function(dt) {
    
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    console.log("inside the enemy render");
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
      this.sprite = 'images/char-boy.png';
-     this.x= 225;
-     this.y=425;
+     this.x= 200;
+     this.y=400;
 };
 
 Player.prototype.update = function(dt) {
@@ -49,37 +43,50 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Player.prototype.reset = function(){
+      player.x=200;
+      player.y=400;
+}
+
 Player.prototype.handleInput = function (key){
     switch (key){
-        case 37:
-            this.x = fafa;
-            //check border and update possition
-            //check colussion.
+        case ("left"):
+            if(this.x-101 >= -101) this.x -= 102 ;
+            logging();
             break;
-        case 37:
-            this.x = fafa;
+
+        case ("up"):
+            if(this.y - 83 >= 0) {
+                this.y -= 83;  
+                logging();         
+                break; 
+            }            
+            else {      
+                this.y -=83;  
+               
+                alert("Congrats - This move lets you get to the safe zone!");                 
+                restart();      
+                break;
+             }
+
+        case ("right"):
+            if(this.x + 101 <= 404) this.x += 101;
+            logging();
             break;
-        case 37:
-            this.x = fafa;
+
+        case ("down"):
+            if(this.y + 83 <= 404) this.y += 83;
+            logging();
             break;
-        case 37:
-            this.x = fafa;
-            break;
+        
     };
 };
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var enemy1 = new Enemy(0,70,10), enemy2= new Enemy(0, 150, 60), enemy3 = new Enemy(0, 225, 10);
+var enemy1 = new Enemy(0,68,70), enemy2= new Enemy(0, 151, 100), enemy3 = new Enemy(0, 234, 180);
 var allEnemies= [ enemy1, enemy2, enemy3];    
 
 var player = new Player();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -87,6 +94,18 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+     player.handleInput(allowedKeys[e.keyCode]);         
+            
 });
+
+var logging = function(){
+    allEnemies.forEach(function(element) {
+        console.log( "Enemy on" + element.x + "," + element.y);
+         }, this);
+         console.log("player is at" + Math.abs(player.x) + "," + player.y);
+}
+
+var restart = function(){
+   var response = prompt("do you want to play again? Type Y if you want to continue playing or N otherwise");
+   if(response === "Y" || response === "y") player.reset();
+}
